c74712209bbece3647251c2112b4c0375ea1a5f2
Yoshiharu Hirose <yh1224@gmail.com>
2018-01-08 23:14:35 +0900

    Modify licenses

--- app/src/main/java/net/assemble/android/mywallet/activity/AboutActivity.kt
+++ app/src/main/java/net/assemble/android/mywallet/activity/AboutActivity.kt
@@ -1,26 +1,74 @@
 package net.assemble.android.mywallet.activity
 
-import android.annotation.SuppressLint
-import android.annotation.TargetApi
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Button
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import com.github.salomonbrys.kodein.instance
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import net.assemble.android.common.activity.BaseActivity
+import net.assemble.android.common.extensions.plusAssign
+import net.assemble.android.common.util.RxBus
 import net.assemble.android.mywallet.R
 import net.assemble.android.mywallet.helper.PackageInfoHelper
+import org.yaml.snakeyaml.Yaml
 
 class AboutActivity : BaseActivity() {
     // Instances injected by Kodein
     private val packageInfoHelper: PackageInfoHelper by instance()
+    private val bus: RxBus by instance()
+
+    /** Disposable container for RxJava */
+    private val disposables = CompositeDisposable()
+
+    class LicenseAdapter(
+            private val data: List<Map<String, Any>>,
+            private val bus: RxBus
+    ) : RecyclerView.Adapter<LicenseAdapter.ItemViewHolder>() {
+        class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val name = itemView.findViewById<TextView>(R.id.name)!!
+            val copyrightHolder = itemView.findViewById<TextView>(R.id.copyrightHolder)!!
+            val url = itemView.findViewById<TextView>(R.id.url)!!
+            val license = itemView.findViewById<TextView>(R.id.license)!!
+        }
+
+        /** クリックイベント */
+        class OnItemClickEvent(val url: String)
+
+        override fun getItemCount() = data.count()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+            val itemView = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.license_info, parent, false)
+            val holder = ItemViewHolder(itemView)
+            itemView.setOnClickListener {
+                val item = data[holder.adapterPosition]
+                val url = item["url"] as String?
+                if (url != null) {
+                    bus.post(LicenseAdapter.OnItemClickEvent(url))
+                }
+            }
+            return holder
+        }
+
+        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+            val item = data[position]
+            holder.name.text = item["name"] as String? ?: ""
+            holder.copyrightHolder.text = item["copyrightHolder"] as String? ?: ""
+            holder.url.text = item["url"] as String? ?: ""
+            holder.license.text = item["license"] as String? ?: ""
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,25 +85,24 @@ class AboutActivity : BaseActivity() {
         version.text = getString(R.string.version, packageInfoHelper.getVersion())
 
         // ライセンス
-        val webView = findViewById<WebView>(R.id.webView)!!
-        webView.webViewClient = object : WebViewClient() {
-            @Suppress("OverridingDeprecatedMember")
-            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
-                return true
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        with(recyclerView) {
+            layoutManager = LinearLayoutManager(this@AboutActivity).apply {
+                orientation = LinearLayoutManager.VERTICAL
             }
+            itemAnimator = DefaultItemAnimator()
 
-            @TargetApi(Build.VERSION_CODES.N)
-            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-                startActivity(Intent(Intent.ACTION_VIEW, request.url))
-
-                return true
+            // Read licenses from Yaml
+            disposables += Single.fromCallable {
+                Yaml().load<List<Map<String, Any>>>(assets.open(LICENSES_YAML))
+                        .filter { (it["skip"] as Boolean?) != true }
             }
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe { licenses ->
+                        adapter = LicenseAdapter(licenses, bus)
+                    }
         }
-        webView.loadUrl(LICENSES_URL)
-        webView.setBackgroundColor(Color.TRANSPARENT)
-        @SuppressLint("SetJavaScriptEnabled")
-        webView.settings.javaScriptEnabled = true
 
         // Support
         //val feedbackButton = findViewById<Button>(R.id.support)
@@ -63,6 +110,17 @@ class AboutActivity : BaseActivity() {
         //    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.support_url)))
         //    startActivity(intent)
         //}
+
+        bus.toObservable()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { event ->
+                    when (event) {
+                        is LicenseAdapter.OnItemClickEvent -> {
+                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(event.url))
+                            startActivity(intent)
+                        }
+                    }
+                }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -70,10 +128,16 @@ class AboutActivity : BaseActivity() {
         return false
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+
+        disposables.dispose()
+    }
+
     companion object {
         @Suppress("unused")
         private val TAG = AboutActivity::class.java.simpleName
 
-        private const val LICENSES_URL = "file:///android_asset/licenses.html"
+        private const val LICENSES_YAML = "licenses.yml"
     }
 }
